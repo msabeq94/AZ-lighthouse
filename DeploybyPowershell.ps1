8a25edc52f698ca80977a9ced62ad613f70f8a21
msabeq94 <msabeq@outlook.com>
2024-07-12 12:19:23 +0300

    Add vf-core-subscription-level1-support group  to LightHouse-subscription.parameters.json

---
+++ DeploybyPowershell.ps1
@@ -0,0 +1,10 @@
+
+
+#
+git clone https://github.com/msabeq94/AZ-lighthouse.git
+New-AzSubscriptionDeployment -Name "VF-LightHouse" -Location "eastus2" -TemplateFile "~/AZ-lighthouse/LightHouse-Subcription.json" -Verbose -TemplateParameterFile "~/AZ-lighthouse/LightHouse-subscription.parameters.json"
+
+# Deploy Azure Resource Manager template that is located externally
+#New-AzSubscriptionDeployment -Name <deploymentName> -Location <AzureRegion> -TemplateUri <templateUri> -Verbose
+
+
