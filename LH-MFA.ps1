756b2eda05a41eed734dfbf5b1d820a0bba93bf9
msabeq94 <msabeq@outlook.com>
2024-08-16 14:11:37 +0300

    Refactor code for improved performance and readability

---
+++ LH-MFA.ps1
@@ -0,0 +1,18 @@
+install-module -Name SecurityFever -Scope CurrentUser
+Import-module -Name SecurityFever
+
+#LH01@tkyaa.com || P@ssw0rd@HL92
+Get-TimeBasedOneTimePassword -SharedSecret 'w7c7bkskf7ydhgvn'
+
+#LH02@tkyaa.com || Poza93631
+Get-TimeBasedOneTimePassword -SharedSecret 'bzb75tgpm2tqjd62'
+
+#LH03@tkyaa.com || P@ssw0rd@HL92
+Get-TimeBasedOneTimePassword -SharedSecret 'dzfdcl6yh5fckqtd'
+
+#LH04@tkyaa.com || P@ssw0rd@HL92
+Get-TimeBasedOneTimePassword -SharedSecret 'ddzlvncrcx6qnmmh'
+
+#LH05@tkyaa.com || P@ssw0rd@HL92  
+Get-TimeBasedOneTimePassword -SharedSecret 'pj5dfbbdwhtmstwl'
+#https://github.com/Azure/Azure-Lighthouse-samples/
