8674f3eb7c276eb69981b64e705abdeef4260250
msabeq94 <msabeq@outlook.com>
2024-08-19 10:57:00 +0300

    Updated deployment script and subscription parameters

--- DeploybyPowershell.ps1
+++ DeploybyPowershell.ps1
@@ -3,7 +3,13 @@
 #
 git clone https://github.com/msabeq94/AZ-lighthouse.git
 
-New-AzSubscriptionDeployment -Name "VF-LightHouse" -Location "eastus2" -TemplateFile "~/AZ-lighthouse/LightHouse-Subcription.json" -Verbose -TemplateParameterFile "~/AZ-lighthouse/LightHouse-subscription.parameters.json"
+New-AzSubscriptionDeployment -Name "VF-LightHouse4" -Location "eastus2" -TemplateFile "~/LH-PCR-VF-IT-PIM_SUBcopy.JSON" -Verbose 
+
+
+
+New-AzSubscriptionDeployment -Name "VF-LightHouse4" -Location "eastus2" -TemplateFile "~/test.JSON" -Verbose 
+
+#-TemplateParameterFile "~/AZ-lighthouse/LightHouse-subscription.parameters.json"
 
 # Deploy Azure Resource Manager template that is located externally
 #New-AzSubscriptionDeployment -Name <deploymentName> -Location <AzureRegion> -TemplateUri <templateUri> -Verbose
